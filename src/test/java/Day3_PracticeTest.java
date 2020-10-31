1ad2f78f99247b52f79934682b30fd979778357c
Ebrish <66562433+ebrish3000@users.noreply.github.com>
2020-10-31 13:43:53 -0700

    message from master branch

--- src/test/java/Day3_PracticeTest.java
+++ src/test/java/Day3_PracticeTest.java
@@ -8,19 +8,8 @@ public class Day3_PracticeTest {
         // line 1
         //  line 2
         // line 3
+       //  message from master branch
 
     }
-    /*
-    Practice :
-        0. checkout master to move your head to master
-        1. Create a new branch called us-123
-        2. Add 2 java class TC001 , TC002
-        3. commit above 2 files
-        4. add some changes into TC001 and commit your work
-        5. add some changes into TC002 and commit your work
-        6. checkout master to move your head back to master
-        7. merge the changes from us-123 branch into master
-        8 observe your log to see the changes are brought to master branch
 
-     */
 }
