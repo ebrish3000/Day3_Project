306ba383917956b75d904884ea47c96dca00feac
Ebrish <66562433+ebrish3000@users.noreply.github.com>
2020-10-31 09:42:56 -0700

    practice commit2

--- src/test/java/Day3_PracticeTest.java
+++ src/test/java/Day3_PracticeTest.java
@@ -2,5 +2,8 @@ public class Day3_PracticeTest {
 
     public static void main(String[] args) {
         System.out.println("Change any thing for git commit test");
+        // CHANGE
+        // another change
+        // this green color is a visual indication to see the change
     }
 }
